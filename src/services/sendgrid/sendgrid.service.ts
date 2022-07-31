6de990ee5dd2abd576522c69631912bbef8668ed
Louis <huynguyennbk@gmail.com>
2022-07-31 10:51:00 +0700

    chore: add from email to environment variable

--- src/services/sendgrid/sendgrid.service.ts
+++ src/services/sendgrid/sendgrid.service.ts
@@ -7,28 +7,28 @@ import { generateEmailTemplate } from '../../templates/email';
 
 @Injectable()
 export class SendgridService {
-  private readonly logger = new Logger(SendgridService.name);
+	private readonly logger = new Logger(SendgridService.name);
 
-  constructor(private readonly configService: ConfigService) {
-    SendGrid.setApiKey(this.configService.get<string>('SEND_GRID_KEY'));
-  }
+	constructor(private readonly configService: ConfigService) {
+		SendGrid.setApiKey(this.configService.get<string>('SEND_GRID_KEY'));
+	}
 
-  async send(data: EmailData) {
-    if (!data) {
-      return;
-    }
+	async send(data: EmailData) {
+		if (!data) {
+			return;
+		}
 
-    const { template_key, to_email } = data;
+		const { template_key, to_email } = data;
 
-    const transport = await SendGrid.send({
-      subject: 'Sign up Verification',
-      from: 'nguyenduclam0605@gmail.com',
-      html: generateEmailTemplate({ templateKey: template_key, data }),
-      to: to_email,
-    });
+		const transport = await SendGrid.send({
+			subject: 'Sign up Verification',
+			from: this.configService.get<string>('SEND_GRID_FROM_EMAIL'),
+			html: generateEmailTemplate({ templateKey: template_key, data }),
+			to: to_email,
+		});
 
-    this.logger.log(`E-Mail sent to ${to_email}`);
+		this.logger.log(`E-Mail sent to ${to_email}`);
 
-    return transport;
-  }
+		return transport;
+	}
 }
